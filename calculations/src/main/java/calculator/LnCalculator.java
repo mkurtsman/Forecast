d731153899edd96497926e33cc0b5025f62ea927
mkurtsman <mkurtsman@gmail.com>
2021-11-07 19:03:24 +0300

    refactor

--- calculations/src/main/java/calculator/LnCalculator.java
+++ calculations/src/main/java/calculator/LnCalculator.java
@@ -39,20 +39,19 @@ public class LnCalculator {
         var normalizedDiff = divide(subtract(diff, center), max.subtract(min).divide(TWO));
 
         // 2. remove cyclic
-        int sinOrder = 3;
+        int sinOrder = 50;
 
         List<BigDecimal> sinParams = IntStream.range(0, sinOrder).mapToObj(i -> BigDecimal.ONE.divide(BigDecimal.valueOf(sinOrder), MathContext.DECIMAL128)).toList();
         List<Range> sinSteps = IntStream.range(0, sinOrder).mapToObj(i -> Range.of(-1.0, 1.0)).toList();
 
-        var sinFunction = new SinFunction(normalizedDiff.size()/2, sinParams);
+        var sinFunction = new SinFunction(normalizedDiff.size()*2, sinParams);
 
         Double sinEps = 0.0000001;
         AbstractOptimizer cyclicOptimizer = new DichotomyParamFuncOptimizer(sinFunction, sinSteps, sinEps, normalizedDiff);
         cyclicOptimizer.optimize();
         cyclicOptimizedRow = apply(normalizedDiff,sinFunction);
-
-        cyclicOptimizedRow = add(mutiply(cyclicOptimizedRow, max.subtract(min).divide(TWO)), center);
         cyclicOptimizedRow = extrapolate(cyclicOptimizedRow, 3, sinFunction);
+        cyclicOptimizedRow = add(mutiply(cyclicOptimizedRow, max.subtract(min).divide(TWO)), center);
 
         ///////////////
 
